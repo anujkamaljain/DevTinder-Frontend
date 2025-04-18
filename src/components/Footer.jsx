c17aa350cd3fbfc14f5a67a96776d4917b1808b2
anujkamaljain <23UCS540@lnmiit.ac.in>
2025-04-18 16:30:13 +0530

    feat: integrate Redux for state management and add user authentication
    - Added Redux Toolkit for state management with user slice for handling user data.
    - Implemented login functionality in the Login component, including API call to authenticate users.
    - Wrapped the application in a Redux Provider to make the store accessible throughout the app.
    - Updated Navbar to display user information when logged in.
    - Created a Feed component for displaying user feeds.
    - Modified Footer component to reflect new branding.
    - Updated ESLint configuration to comment out unused imports.
    - Updated package.json and package-lock.json to include new dependencies: axios, @reduxjs/toolkit, and react-redux.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <footer className="footer footer-horizontal footer-center bg-base-300 text-primary-content p-3 fixed bottom-0 mt-10">
+        <footer className="footer footer-horizontal footer-center bg-base-300 text-primary-content p-3 mt-10 ">
   <aside>
     <img src='/Logo.png' className='w-18 h-18'/>
     <p className="font-bold">
-      ACME Industries Ltd.
+      DevTinder Pvt. Ltd.
       <br />
-      Providing reliable tech since 1992
+      Providing reliable tech since 2025
     </p>
     <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
   </aside>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 function Login() {
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+
+  const [Email, setEmail] = React.useState("");
+  const [Password, setPassword] = React.useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(BASE_URL + "/login", {
+        emailId: Email,
+        password: Password
+      }, {withCredentials: true});
+      dispatch(addUser(res.data));
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset w-xs bg-base-300 border border-base-300 p-5 rounded-box">
+              <label className="fieldset-label">Email ID</label>
+              <input
+                type="email"
+                className="input"
+                placeholder="Email"
+                value={Email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <label className="fieldset-label">Password </label>
+              <input
+                type="password"
+                className="input"
+                placeholder="Password"
+                value={Password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button className="btn btn-primary mt-4" onClick={handleLogin}>
+                Login
+              </button>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
